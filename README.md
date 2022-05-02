435d630bdaa07cafa04c040c74f8a961b000831d
shhossain <hossain0338@gmail.com>
2022-05-02 18:00:17 +0000

    package created

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# colab_connect
+# Colab Connect helps you connect with your colleagues in Google Colab
---
+++ setup.py
@@ -0,0 +1,27 @@
+# create a setup script for colab_connect
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colab_connect",
+    version="0.0.1",
+    author="Sifat",
+    author_email="hossain0338@gmail.com",
+    description="Colab Connect",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hossain0338/colab_connect",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    project_urls={
+        "Bug Tracker": "https://github.com/hossain0338/colab_connect/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
